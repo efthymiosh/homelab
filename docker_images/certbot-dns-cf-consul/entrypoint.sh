838b06cd66a0da047958221cdd656830457a8eef
Efthymios Hadjimichael <ehadjimichael@id5.io>
2024-10-21 12:43:19 +0300

    Fix certgen

--- docker_images/certbot-dns-cf-consul/entrypoint.sh
+++ docker_images/certbot-dns-cf-consul/entrypoint.sh
@@ -9,6 +9,8 @@ for KEY in cert chain fullchain privkey; do
 done
 
 
-for KEY in cert chain fullchain privkey; do
-    vault kv patch -mount=kv nomad/shared/generated/ssl/$SSL_DOMAIN $KEY="@/etc/letsencrypt/live/$SSL_DOMAIN/$KEY.pem"
-done
+vault kv put -mount=kv nomad/shared/generated/ssl/$SSL_DOMAIN \
+  cert="@/etc/letsencrypt/live/$SSL_DOMAIN/cert.pem" \
+  chain="@/etc/letsencrypt/live/$SSL_DOMAIN/chain.pem" \
+  fullchain="@/etc/letsencrypt/live/$SSL_DOMAIN/fullchain.pem" \
+  privkey="@/etc/letsencrypt/live/$SSL_DOMAIN/privkey.pem"
